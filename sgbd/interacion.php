82382942f5c2e5ab4f79cdfb5c502812b1352ed7
bryanpumax <50306574+bryanpumax@users.noreply.github.com>
2020-12-25 01:54:20 -0500

    Se  agrego  plugins  y boostrap  para  interfaz

--- sgbd/interacion.php
+++ sgbd/interacion.php
@@ -1,28 +1,36 @@
 <?php 
-include "mysql.php";
-echo consultas("gg","*","where gt=1");
+ 
 function consultas($tabla,$campo,$texto_condicion)
-{
-
-        return "SELECT $campo from $tabla $texto_condicion;";
+{include "mysql.php";
+$sentecia="SELECT $campo from $tabla $texto_condicion;";
+ $sql = $pdo->query($sentecia);
+return $sql;
 }
 function insertar($tabla,$campo,$texto)
-{
-        return "INSERT into $tabla ($campo) values ($texto)";
-
+{include "mysql.php";
+        
+$sentecia= "INSERT into $tabla ($campo) values ($texto)";
+ $sql = $pdo->query($sentecia);
+return $sql;
 }
 function update($tabla,$campo_valor,$texto)
-{
-        return "UPDATE $tabla SET $campo_valor where $texto";
-
+{include "mysql.php";
+    $sentecia= "UPDATE $tabla SET $campo_valor where $texto";
+ 
+ $sql = $pdo->query($sentecia);
+return $sql;
 }
 function delete($tabla,$campo_valor)
-{
-        return "DELETE FROM $tabla where $campo_valor";
+{include "mysql.php";
+       $sentecia=  "DELETE FROM $tabla where $campo_valor";
 
+ $sql = $pdo->query($sentecia);
+return $sql;
 }
 function procedimiento($procedimiento,$valor)
-{
+{include "mysql.php";
 //valor debes enviarle  con una  cadena  de texto
-        return "Call $procedimiento($valor);";
+        $sentecia= "Call $procedimiento($valor);"; 
+ $sql = $pdo->query($sentecia);
+return $sql;
 }
